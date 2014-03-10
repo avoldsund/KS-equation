ce3332eb497b5176c4706bf2f3a1b8ddf8dc3694
Elisabeth Skaar Hasund <karieh@stud.ntnu.no>
2014-03-10 15:59:00 +0100

    lots of matlab

--- ks_impexp.m
+++ ks_impexp.m
@@ -2,15 +2,45 @@
 
 f = @(x) cos(x/16).*(1+sin(x/16));
 
-M = 2^6+1;
-x = linspace(0,32*pi,M);
+M = 128;
+%x = linspace(0,32*pi,M);
+x = 32*pi*(1:M)/M;
 h = (32*pi)/(M-1);
-T = 20;
-k = 0.0001;
-N = ceil(T/k);
+k = 0.025;
+N = 15000;
+T = N*k;
+%T = 20;
+%N = ceil(T/k);
 
+% Generating matrices for imp.exp. method
 w = ones(1,M-1);
-Axx = sparse(-2*eye(M) + diag(w,1) + diag(w,-1));
-Axx(1,M) = 1;
-Axx(M,1) = 1;
-Axx = k/(h^2)*Axx;
+A = sparse(-2*eye(M) + diag(w,1) + diag(w,-1));
+A(1,M) = 1;
+A(M,1) = 1;
+A = k/(h^2)*A;
+
+u = ones(1,M-2);
+v = -4*ones(1,M-1);
+B = sparse(6*eye(M)+diag(v,1)+diag(v,-1)+diag(u,2)+diag(u,-2)+diag([1;1],M-2)+diag([1;1],-M+2));
+B(1,M) = -4;
+B(M,1) = -4;
+B = k/(h^4)*B;
+
+D = sparse(zeros(M) + diag(w,1) + diag(-w,-1));
+D(M,1) = 1;
+D(1,M) = -1;
+D = k/(4*h)*D;
+
+F = (eye(M)+A+B);
+G = (eye(M)-A-B);
+
+U = f(x);
+
+% Time step N iterations:
+for n = 1:N
+    U(n+1,:) = F\G*(U(n,:)') - F\D*(U(n,:)'.^2);
+end
+
+%mesh(U)
+figure()
+contourf(U)
