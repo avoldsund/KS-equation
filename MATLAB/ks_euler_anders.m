500413459e45bd57c236ac6a4a2205210575b5a2
Anders Opskar Voldsund <anderovo@null12.math.ntnu.no>
2014-03-30 17:07:43 +0200

    Deleted some files.

--- MATLAB/ks_euler_anders.m
+++ MATLAB/ks_euler_anders.m
@@ -1,21 +1,15 @@
-tic
+function [U h x k] = ks_euler_anders(length, M, time, N)
+% ks_euler_anders(length, M, time, N)
 % Implementation of the Kuramoto-Sivashinsky equation
 % u_t + u_xxxx + u_xx + uu_x = 0
 % Central differences in space, forwards in time
 
 % Initial values
-length = 32*pi;
-M = 128;
-h = length/(M);
-
-time = 300;
-N = 30000;
+h = length/M;
 k = time/N;
 
-% x = h * (1:M)
-% x = 32*pi/(M-1) * (1:M)
-x = (32*pi)*(1:M)/(M);
-t = k * (0:N);
+x = h * (0:M-1);
+t = k * (0:N-1);
 
 % Boundary conditions
 % u(x,0) = f(x)
@@ -38,12 +32,28 @@ Axx = (k/(h^2)) * spdiags([e e -2*e e e], diagVecAxx, M, M);
 diagVecB = [-M+1 -1 1 M-1];
 B = (k/(4*h)) * spdiags([1*e -1*e 1*e -1*e], diagVecB, M, M);
 
-% Running 
-for i = 1:N
+% For-loop to calculate the next time step
+for i = 1:N-1
     U(:,i+1) = U(:,i) - Axxxx*U(:,i) - Axx*U(:,i) - B*(U(:,i).^2);
 end
+% 
+% [V, xa, ya] = compress(t, x, U, 1000, 1000);
+% 
+% figure
+% mesh(xa,ya,V')
+% %view(230,45)
+% view(340,60)
+% %view(90,270) Contour plot of the solution
+% title('Numerical solution of the KS-eq: M = 128, N = 2^{16}')
+% ylabel('time')
+% xlabel('space [0, 32*pi]')
 
-figure
-%contourf(t, x, U)
-contourf(U')
-toc
+% figure
+% surf(t,x,U)
+% title('Numerical solution for KS-equation')
+% xlabel('time')
+% ylabel('space')
+% view(90,270)
+% shading FLAT
+% %contourf(t,x,U)
+end
