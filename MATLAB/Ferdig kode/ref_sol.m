75380c2ca0e4b06986bc7f8b8094f8b7d6af6547
Anders Opskar Voldsund <anderovo@null12.math.ntnu.no>
2014-03-30 15:56:55 +0200

    Added compressfile and pdfsave-file

--- MATLAB/Ferdig kode/ref_sol.m
+++ MATLAB/Ferdig kode/ref_sol.m
@@ -3,7 +3,7 @@ function yy = ref_sol(k,T,x)
 f = @(x) cos(x/16).*(1+sin(x/16));
 
 y0 = f(x);
-options = odeset('AbsTol', 1e-3, 'RelTol', 1e-3);
+options = odeset('AbsTol', 1e-12, 'RelTol', 1e-12);
 [~,yy] = ode15s('funcKS', 0:k:T, y0, options);
 yy = yy';
 
